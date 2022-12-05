b5e571017836adf63d0d64197fb1493be124def0
shwethanewaccount123 <98377345+shwethanewaccount123@users.noreply.github.com>
2022-12-05 12:54:33 +0530

    Update SpringBootLazyInitExampleApplication.java

--- src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Comaponent(basepPackage={"com.techprimers.lazy.controller"})
 public class SpringBootLazyInitExampleApplication {
 
 	public static void main(String[] args) {
